c2fc911b84bcfb786c53b7b56c6bb249ac27560a
Brian Ho <brian.dungho@gmail.com>
2025-05-30 11:36:42 -0400

    feat(config): add environment-based configuration structure
    Introduced multi-level config setup to support 'dev' and 'pro' environments.
    - Level 0: Base config (initial structure)
    - Level 1: Added separate dev1 and pro1 configs
    - Level 2: Integrated environment variables for dynamic config values
    - Automatically selects config based on NODE_ENV (defaults to 'dev')
    
    This enhances flexibility and prepares the app for deployment in different environments.

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 // server.js
 import app from './src/app.js'
 import { connectDB, client } from './src/database/init.mongodb.js'
-import checkActiveConnections from './src/helpers/checkActiveConnections.js'
 
 const PORT = process.env.PORT || 3055
 
 connectDB().then(() => {
-  checkActiveConnections()
   const server = app.listen(PORT, () => {
     console.log(`🚀 Server is running at http://localhost:${PORT}`)
   })
---
+++ src/configs/config.mongodb.js
@@ -0,0 +1,69 @@
+'use strict'
+// Level 0
+
+const config1 = {
+    app: {
+        port: 3000
+    },
+    db: {
+        host: 'localhost',
+        port: 27017,
+        name: 'db'
+    }
+}
+
+
+// Level 1
+
+const dev1 = {
+    app: {
+        port: 3000
+    },
+    db: {
+        host: 'localhost',
+        port: 27017,
+        name: 'dbDev'
+    }
+}
+
+const pro1 = {
+    app: {
+        port: 3000
+    },
+    db: {
+        host: 'localhost',
+        port: 27017,
+        name: 'dbProduct'
+    }
+}
+
+// const config = { dev, pro }
+// const env = process.env.NODE_ENV || 'dev'
+// export default config[env]
+// Level 2 
+
+const dev = {
+    app: {
+        port: process.env.DEV_APP_PORT || 3000
+    },
+    db: {
+        host: process.env.DEV_DB_HOST || 'localhost',
+        port: process.env.DEV_DB_PORT || 27017, 
+        name: process.env.DEV_DB_NAME || 'dbDev'
+    }
+}
+
+const pro = {
+    app: {
+        port: process.env.PRO_APP_PORT || 3000
+    },
+    db: {
+        host: process.env.PRO_DB_HOST || 'localhost',
+        port: process.env.PRO_DB_PORT || 27017,
+        name: process.env.PRO_DB_NAME || 'dbProduct'
+    }
+}
+
+const config = { dev, pro }
+const env = process.env.NODE_ENV || 'dev'
+export default config[env]
